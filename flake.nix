48d64f9bd77c7ebe8a01a2f8d0174b8119edaf7c
Thomas Boerger <thomas@webhippie.de>
2022-11-30 11:18:03 +0100

    feat: add nix flake and fix deprecation messages

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Nix flake for development";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    };
+
+    utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            gcc
+            glibc.static
+            go
+          ];
+        };
+      }
+    );
+}
