8d4065a3e643af192c8c2e343127291b56fbba6a
Thomas Boerger <thomas@webhippie.de>
2025-04-20 12:51:01 +0200

    fix: switch back to int instead of int64 because of cli

--- pkg/action/server.go
+++ pkg/action/server.go
@@ -84,7 +84,7 @@ func Server(cfg *config.Config, logger *slog.Logger) error {
 		disc := Discoverer{
 			configs:   configs,
 			logger:    logger,
-			refresh:   int(cfg.Target.Refresh),
+			refresh:   cfg.Target.Refresh,
 			separator: ",",
 			lasts:     make(map[string]struct{}),
 		}
